196f2906777d52df4df58ff6294781d391951608
sammienjihia <dilaninjihia@gmail.com>
2019-01-14 19:13:28 +0300

    First commit

---
+++ hello_world/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using hello_world;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hello_world.newClass exClass = new newClass();
+
+            exClass.firstname = "Tony";
+            exClass.middlename = "velvet";
+            exClass.lastname = "Wright";
+            exClass.age = 20;
+            exClass.city = "Jinja";
+            exClass.country = "Uganda";
+            exClass.street = "17th street, Brookside Drive";
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, my names are {0} {1} {2}", exClass.firstname, exClass.middlename
+            , exClass.lastname );
+        }
+    }
+}
---
+++ hello_world/some_new_file.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace hello_world 
+{
+    class newClass
+    {
+        public string firstname = "";
+        public string lastname = "";
+        public string middlename = "";
+
+        public int age = 0;
+        public string street = "";
+        public string city = "";
+        public string country ="";
+        public DateTime date_of_birth;
+        
+    }
+}
